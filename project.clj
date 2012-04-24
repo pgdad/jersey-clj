55e5f216b40baa508dae92468eda02fc272498fd
pgdad <pgdad@hotmail.com>
2012-04-24 17:06:19 -0400

    newer version of jetty

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject jersey-clj "1.0.1"
+(defproject jersey-clj "1.0.2"
   :description "Simple way to implement jersey restful services with clojure"
   :aot [jersey.CljJerseyServletContainer jersey.Hello]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.sun.jersey/jersey-servlet "1.12"]]
-  :dev-dependencies [[org.eclipse.jetty/jetty-servlet "8.1.2.v20120308"]]
+  :dev-dependencies [[org.eclipse.jetty/jetty-servlet "8.1.3.v20120416"]]
   :java-source-path "test/java"
   :javac-options {:debug "true" :destdir "classes/"})
